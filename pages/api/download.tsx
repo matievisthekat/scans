e7aed94a4471bcee927b4e9346d5b84131282158
matievisthekat <matievisthekat@gmail.com>
2022-09-28 18:49:04 +0200

    resolve api promises

--- pages/api/download.tsx
+++ pages/api/download.tsx
@@ -4,32 +4,34 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, child, get } from "firebase/database";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-	const app = initializeApp({
-		apiKey: process.env.API_KEY,
-		authDomain: "scans-3ddfc.firebaseapp.com",
-		projectId: "scans-3ddfc",
-		storageBucket: "scans-3ddfc.appspot.com",
-		messagingSenderId: "896495032329",
-		appId: "1:896495032329:web:1fedc96a82a623124940d8",
-		measurementId: "G-09EJ54SYZG",
-		databaseURL: "https://scans-3ddfc-default-rtdb.europe-west1.firebasedatabase.app/"
-	});
-
-	const db = getDatabase(app);
-
-	const email = req.query.email as string;
-	if (!email) {
-		res.status(400).json({message: "Please provide an email address"});
-		return {};
-	}
-
-	get(child(ref(db), `users/${email.replace(".", "_")}`)).then((result) => {
-		if (result.exists()) {
-			res.status(200).send(JSON.stringify(result.val(), null, 2));
-		} else {
-			res.status(404).json({message: "No user found with that email address"});
+	return new Promise<void>((resolve, reject) => {
+		const app = initializeApp({
+			apiKey: process.env.API_KEY,
+			authDomain: "scans-3ddfc.firebaseapp.com",
+			projectId: "scans-3ddfc",
+			storageBucket: "scans-3ddfc.appspot.com",
+			messagingSenderId: "896495032329",
+			appId: "1:896495032329:web:1fedc96a82a623124940d8",
+			measurementId: "G-09EJ54SYZG",
+			databaseURL: "https://scans-3ddfc-default-rtdb.europe-west1.firebasedatabase.app/"
+		});
+	
+		const db = getDatabase(app);
+	
+		const email = req.query.email as string;
+		if (!email) {
+			res.status(400).send("Please provide an email address");
+			return {};
 		}
+	
+		get(child(ref(db), `users/${email.replace(".", "_")}`)).then((result) => {
+			if (result.exists()) {
+				res.status(200).send(JSON.stringify(result.val(), null, 2));
+				resolve();
+			} else {
+				res.status(404).send("No user found with that email address");
+				reject();
+			}
+		});
 	});
-
-	return {};
 }
--- pages/api/save.tsx
+++ pages/api/save.tsx
@@ -4,26 +4,27 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set } from "firebase/database";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-	const app = initializeApp({
-		apiKey: process.env.API_KEY,
-		authDomain: "scans-3ddfc.firebaseapp.com",
-		projectId: "scans-3ddfc",
-		storageBucket: "scans-3ddfc.appspot.com",
-		messagingSenderId: "896495032329",
-		appId: "1:896495032329:web:1fedc96a82a623124940d8",
-		measurementId: "G-09EJ54SYZG",
-		databaseURL: "https://scans-3ddfc-default-rtdb.europe-west1.firebasedatabase.app/"
-	});
-
-	const db = getDatabase(app);
-
-  const data = JSON.parse(req.body);
-  
-  set(ref(db, `users/${data.email.replace(".", "_")}`), {
-    totals: data.totals
-  });
-
-  res.status(200).json({success:true});
+	return new Promise<void>((resolve, reject) => {
+		const app = initializeApp({
+			apiKey: process.env.API_KEY,
+			authDomain: "scans-3ddfc.firebaseapp.com",
+			projectId: "scans-3ddfc",
+			storageBucket: "scans-3ddfc.appspot.com",
+			messagingSenderId: "896495032329",
+			appId: "1:896495032329:web:1fedc96a82a623124940d8",
+			measurementId: "G-09EJ54SYZG",
+			databaseURL: "https://scans-3ddfc-default-rtdb.europe-west1.firebasedatabase.app/"
+		});
 	
-  return {};
+		const db = getDatabase(app);
+	
+		const data = JSON.parse(req.body);
+		
+		set(ref(db, `users/${data.email.replace(".", "_")}`), {
+			totals: data.totals
+		});
+	
+		res.status(200);
+		resolve();
+	});
 }
